276e420e4fbf23cdf90a2db0bf7f01d8c4adb33c
Vaisakh-RS <vaisakhrs4@gmail.com>
2023-08-22 19:44:02 +0530

    Add a function which removes the given number of numbers from an array
    Learnt about rest operator

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+// ...is the rest operator which combines all the arguments after the array to be into another array
 
+const removeFromArray = function(arr,...numbersToBeRemoved) {
+    const newArray=[];
+    arr.forEach(element => {
+        if(!numbersToBeRemoved.includes(element))
+        {
+            newArray.push(element);
+        }
+    });
+
+    return newArray;
 };
 
+console.log(removeFromArray([1,2,3],2))
 // Do not edit below this line
 module.exports = removeFromArray;
