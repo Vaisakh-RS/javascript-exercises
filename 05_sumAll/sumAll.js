9b34fe270453ecfd45a1f3b3bfc5952434bb7482
Vaisakh-RS <vaisakhrs4@gmail.com>
2023-08-22 19:49:06 +0530

    Add a function to find the sum of the numbers between an interval

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
+const sumAll = function(startNumber,endNumber) {
+    let sum=0;
+    for(let i=startNumber;i<=endNumber;i++)
+    {
+        sum+=i;
+    }
 
+    return sum
 };
 
+console.log(sumAll(1,8))
 // Do not edit below this line
 module.exports = sumAll;
