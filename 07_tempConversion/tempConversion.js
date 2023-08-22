51cf41b7cb1fc61e5129a5afb4d98410fbc317a5
Vaisakh-RS <vaisakhrs4@gmail.com>
2023-08-22 21:07:02 +0530

    Add 2 functions for temperature conversion and round them to one decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,20 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(farenheit) {
+  //(f-32)/1.8
+  digits=1;
+  celcius=(farenheit-32)/1.8;
+  return(Math.round((celcius*Math.pow(10,digits)).toFixed(digits-1))/Math.pow(10,digits)).toFixed(digits);
+ 
+}
 
-const convertToFahrenheit = function() {
-};
 
+const convertToFahrenheit = function(celcius) {
+  digits=1;
+  farenheit=(celcius*1.8)+32;
+  return(Math.round((farenheit*Math.pow(10,digits)).toFixed(digits-1))/Math.pow(10,digits)).toFixed(digits);
+}
+
+console.log(convertToCelsius(32));
+console.log(convertToFahrenheit(0));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
